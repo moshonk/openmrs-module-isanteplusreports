9549652417a9fcab092415d560dde3491e658efe
moshonk <omondi.ken@gmail.com>
2020-01-17 06:23:36 +0300

    Added cohort indicator for ART Enrollment among medically eligible single indicator report

---
+++ api/src/main/java/org/openmrs/module/isanteplusreports/library/indicator/SingleIndicatorReportIndicatorLibrary.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.isanteplusreports.library.indicator;
+
+import org.openmrs.module.isanteplusreports.library.cohort.SingleIndicatorReportCohortLibrary;
+import org.openmrs.module.isanteplusreports.reporting.utils.EmrReportingUtils;
+import org.openmrs.module.isanteplusreports.reporting.utils.ReportUtils;
+import org.openmrs.module.reporting.indicator.CohortIndicator;
+
+public class SingleIndicatorReportIndicatorLibrary {
+	
+	private static final String IND_PARAMS = "startDate=${startDate},endDate=${endDate},location=${location}";
+	
+	public static CohortIndicator artEnrollmentsAmongMedicallyEligible() {
+		return EmrReportingUtils.cohortIndicator("isanteplusreports.art_enrollments_among_medically_eligible",
+		    ReportUtils.map(SingleIndicatorReportCohortLibrary.artEnrollmentAmongMedicallyEligible(),
+		        IND_PARAMS));
+	}
+	
+}
